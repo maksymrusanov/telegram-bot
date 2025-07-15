ec8b12ab1ac47b4f86de16c717da6f503b027064
maksym <1maksymrusanov1@gmai.com>
2025-07-15 20:10:24 +0100

    create logic to sort offers on spareroom

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-driver=webdriver.Firefox()
-driver.get('https://www.spareroom.co.uk/')
-search=driver.find_element(By.NAME,'search')
-search.send_keys('london')
-search.send_keys(Keys.ENTER)
-price=driver.find_element(By.NAME,'max_rent')
-price.send_keys('600')
-apply=driver.find_element(By.CLASS_NAME,'search_filter_submit')
-apply.click()
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def find_offers():
+    driver = webdriver.Firefox()
+    driver.get('https://www.spareroom.co.uk/')
+    search = driver.find_element(By.NAME, 'search')
+    search.send_keys('london')
+    search.send_keys(Keys.ENTER)
+    price = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "maxRent")))
+    price.send_keys('600')
+    print(price.text)
+    apply = driver.find_element(
+        By.CSS_SELECTOR, '#searchFilters > div > div > div > button')
+    apply.click()
+
+
+find_offers()
