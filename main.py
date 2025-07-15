4a885049ccb34f1d199dddde6a7d0d8bfaf46f5e
Maksym <1maksymrusanov1@gmail.com>
2025-07-15 19:12:22 +0100

    add some func

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import telebot
-bot=telebot.TeleBot('7954199406:AAFisYc8lOMOqBHmske-LtscKqlj-49Bae0')
-@bot.message_handler(commands=['start'])
-def main(message):
-    bot.reply_to(message,'hello')
-
-bot.infinity_polling()
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+driver=webdriver.Firefox()
+driver.get('https://www.spareroom.co.uk/')
+search=driver.find_element(By.NAME,'search')
+search.send_keys('london')
+search.send_keys(Keys.ENTER)
+price=driver.find_element(By.NAME,'max_rent')
+price.send_keys('600')
+apply=driver.find_element(By.CLASS_NAME,'search_filter_submit')
+apply.click()
